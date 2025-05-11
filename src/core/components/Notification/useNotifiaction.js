054992abd4579a352e181b64c5f31047be56265a
Bholuvivek <viveksinghpihuli0a@gmaill.com>
2025-05-11 13:32:48 +0530

    remove old code from main branch

--- src/core/components/Notification/useNotifiaction.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { NotificationContext } from './NotificationProvider';
-
-const useNotification = () => {
-  //notification context
-  return useContext(NotificationContext);
-};
-
-export default useNotification;
--- src/core/customHooks/useToggle.js
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-
-export const useToggle = (initialState = false) => {
-  const [value, setValue] = useState(initialState);
-  const toggle = () => setValue((prev) => !prev);
-  return [value, toggle];
-};
